0860bb59eae39f996b84ba2e7d9008d50b03931a
oIthalo <ithalobarreto333@gmail.com>
2026-02-20 03:11:17 -0300

    refact: refatoracao UI auth

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
+import { RegisterRequest } from '../../../requests/auth/register.request';
+import { AuthResponse } from '../../../responses/auth/auth.response';
+import { ErrorResponse } from '../../../responses/default/error.response';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  request: RegisterRequest = {
+    username: "",
+    email: "",
+    password: "",
+    keepalive: false
+  };
 
+  successResponse: AuthResponse = {
+    email: "",
+    username: "",
+    tokens: {
+      accessToken: "",
+      refreshToken: ""
+    }
+  };
+
+  errorResponse: ErrorResponse = {
+    errorMessage: "",
+    errorCode: "",
+    statusCode: 0,
+    errors: []
+  }
+
+  isLoading = false
+  isError = false
 }
